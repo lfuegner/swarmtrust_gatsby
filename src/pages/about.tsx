2ba2867ab20607920e989c7f7f3b983ce94a6986
Ludwig Fügner Laptop <lfuegner@gevb.de>
2023-01-26 19:42:40 +0100

    about.tsx function created (#2)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,14 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
 
+const AboutPage: React.FC<PageProps> = () => {
+    return(
+        <main>
+            <h1>Meine erste About page mit Gatsby und react</h1>
+        </main>
+    )
+}
+
+export default AboutPage
+
 export const Head: HeadFC = () => <title>About</title>
