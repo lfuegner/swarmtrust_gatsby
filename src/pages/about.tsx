112e1615a977717541650eb902912d5ac363dcd9
Ludwig Fügner Laptop <lfuegner@gevb.de>
2023-01-26 20:25:24 +0100

    trying out gatsby (#3)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,13 +2,22 @@ import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
 
 const AboutPage: React.FC<PageProps> = () => {
+    console.log('Hello World')
     return(
         <main>
-            <h1>Meine erste About page mit Gatsby und react</h1>
+            <h1>About Me</h1>
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            <Link to="/">Back to Home</Link>
         </main>
     )
 }
 
+
 export default AboutPage
 
-export const Head: HeadFC = () => <title>About</title>
+export const Head: HeadFC = () => (
+    <>
+    <title>About</title>
+    <meta name="description" content="Your description"/>
+    </>
+)
