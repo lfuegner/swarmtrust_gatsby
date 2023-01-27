c649f652a9f23af75355fb7fa6b1b870c5a51e26
Ludwig Fügner Laptop <lfuegner@gevb.de>
2023-01-27 18:26:24 +0100

    scss folder structure tested (#4)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,13 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
+import Header from '../components/header'
+import "../styles/styles.scss"
 
 const AboutPage: React.FC<PageProps> = () => {
     console.log('Hello World')
     return(
         <main>
+            <Header />
             <h1>About Me</h1>
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
             <Link to="/">Back to Home</Link>
